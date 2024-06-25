a894c298c2465ef3a2a1ed912585ebbd6ad1101f
anya-mb <anyamb@yandex.ru>
2024-06-25 13:38:48 -0700

    added description, amenities and house_rules tests

--- src/tests/test_parse_listings_info.py
+++ src/tests/test_parse_listings_info.py
@@ -7,18 +7,27 @@ from ..data_loading.parse_listings_info import safe_get, replace_line_breaks, \
     get_listing_presentation, parse_house_rules, parse_amenities, parse_description
 from ..data_loading.utils import read_jsonl
 
-# Sample data from the .jsonl file
-sample_file_path = os.path.join(os.path.dirname(__file__), 'test_data', '172222.jsonl')
-sample_data = read_jsonl(sample_file_path)
-sample_listing_info_json = sample_data
+# Sample data from downloaded jsons out of htmls
+sample_file_path_172222 = os.path.join(os.path.dirname(__file__), 
+                                       'test_data', 
+                                       '172222.jsonl')
+sample_listing_info_json_172222 = read_jsonl(sample_file_path_172222)
 
-expected_parsed_result_file_path = os.path.join(os.path.dirname(__file__), 'test_data', '172222_parsed_result.jsonl')
-parsed_listing_data = read_jsonl(expected_parsed_result_file_path)[0]
+sample_file_path_888757305688084444 = os.path.join(os.path.dirname(__file__), 
+                                                   'test_data', 
+                                                   '888757305688084444.jsonl')
+sample_listing_info_json_888757305688084444 = read_jsonl(sample_file_path_888757305688084444)
 
+# Parsed listing data 
+expected_parsed_result_file_path_172222 = os.path.join(os.path.dirname(__file__), 
+                                                       'test_data', 
+                                                       '172222_parsed_result.jsonl')
+parsed_listing_data_172222 = read_jsonl(expected_parsed_result_file_path_172222)[0]
 
-
-# # Sample presentation extracted from sample data
-# sample_presentation = sample_listing_info_json[0]['presentation']
+expected_parsed_result_file_path_888757305688084444 = os.path.join(os.path.dirname(__file__), 
+                                                                   'test_data', 
+                                                                   '888757305688084444_parsed_result.jsonl')
+parsed_listing_data_888757305688084444 = read_jsonl(expected_parsed_result_file_path_888757305688084444)[0]
 
 
 def test_safe_get():
@@ -34,12 +43,29 @@ def test_replace_line_breaks():
 
 
 def test_get_listing_presentation():
-    expected_presentation_file_path = os.path.join(os.path.dirname(__file__), 'test_data', '172222_expected_presentation.jsonl')
+    expected_presentation_file_path = os.path.join(os.path.dirname(__file__), 
+                                                   'test_data', 
+                                                   '172222_expected_presentation.jsonl')
     expected_presentation = read_jsonl(expected_presentation_file_path)[0]
 
-    result_presentation = get_listing_presentation(sample_listing_info_json, "test_id")
+    result_presentation = get_listing_presentation(sample_listing_info_json_172222, "test_id")
 
     assert result_presentation == expected_presentation, (
+        f"Listing id: 172222\n"
+        f"Failed to get listing presentation correctly.\n"
+        f"Expected: {expected_presentation}\n"
+        f"Got: {result_presentation}"
+    )
+
+    expected_presentation_file_path = os.path.join(os.path.dirname(__file__), 
+                                                   'test_data', 
+                                                   '888757305688084444_expected_presentation.jsonl')
+    expected_presentation = read_jsonl(expected_presentation_file_path)[0]
+
+    result_presentation = get_listing_presentation(sample_listing_info_json_888757305688084444, "test_id")
+
+    assert result_presentation == expected_presentation, (
+        f"Listing id: 888757305688084444\n"
         f"Failed to get listing presentation correctly.\n"
         f"Expected: {expected_presentation}\n"
         f"Got: {result_presentation}"
@@ -47,13 +73,27 @@ def test_get_listing_presentation():
 
 
 def test_parse_house_rules():
-    presentation = get_listing_presentation(sample_listing_info_json, "test_id")
+    presentation = get_listing_presentation(sample_listing_info_json_172222, "test_id")
     result_house_rules = parse_house_rules("test_id", presentation)
 
-    expected_house_rules = dict((key, parsed_listing_data[key]) 
-                                for key in parsed_listing_data.keys() if key.endswith('_house_rule'))
+    expected_house_rules = dict((key, parsed_listing_data_172222[key]) 
+                                for key in parsed_listing_data_172222.keys() if key.endswith('_house_rule'))
 
     assert result_house_rules == expected_house_rules,  (
+        f"Listing id: 172222\n"
+        f"Failed to parse house rules correctly.\n"
+        f"Expected: {expected_house_rules}\n"
+        f"Got: {result_house_rules}"
+    )
+
+    presentation = get_listing_presentation(sample_listing_info_json_888757305688084444, "test_id")
+    result_house_rules = parse_house_rules("test_id", presentation)
+
+    expected_house_rules = dict((key, parsed_listing_data_888757305688084444[key]) 
+                                for key in parsed_listing_data_888757305688084444.keys() if key.endswith('_house_rule'))
+
+    assert result_house_rules == expected_house_rules,  (
+        f"Listing id: 888757305688084444\n"
         f"Failed to parse house rules correctly.\n"
         f"Expected: {expected_house_rules}\n"
         f"Got: {result_house_rules}"
@@ -68,6 +108,7 @@ def test_parse_amenities():
                               for key in parsed_listing_data.keys() if key.endswith('_amenities'))
 
     assert result_amenities == expected_amenities, (
+        f"Listing id: 172222\n"
         f"Failed to parse amenities correctly.\n"
         f"Expected: {expected_amenities}\n"
         f"Got: {result_amenities}"
@@ -82,6 +123,7 @@ def test_parse_description():
                                 for key in parsed_listing_data.keys() if key.endswith('_description'))
 
     assert result_description == expected_description, (
+        f"Listing id: 172222\n"
         f"Failed to parse description correctly.\n"
         f"Expected: {expected_description}\n"
         f"Got: {result_description}"
